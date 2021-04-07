c759dd2e4ded3f64a3da55403b809af5057078b6
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2021-04-07 18:28:26 +0100

    Factor out fetching the last-run timestamp.
    Also don't bother calling HEAD on the bucket just to check that it
    exists; we already get an informative error message if we fail to read
    from the bucket.

--- terraform/lambda/RDSLogsToS3/main.py
+++ terraform/lambda/RDSLogsToS3/main.py
@@ -5,6 +5,23 @@ import os
 import posixpath
 
 
+def get_last_received_timestamp(s3_client, bucket_name, key):
+    """Read an integer from a file in S3 or return 0 if not found."""
+    print(f"Reading last received timestamp from s3://{bucket_name}/{key}")
+    try:
+        obj = s3_client.get_object(Bucket=bucket_name, Key=key)
+        timestamp = int(obj["Body"].read())
+        print(f"Retrieving log files with LastWritten time after {timestamp}")
+        return timestamp
+    except botocore.exceptions.ClientError as e:
+        error_code = int(e.response["ResponseMetadata"]["HTTPStatusCode"])
+        if error_code == 404:
+            print("No last received timestamp file found. All files will be retrieved from RDS.")
+            return 0
+        else:
+            raise Exception(f"Unable to access s3://{bucket_name}/{key}: {e}")
+
+
 def lambda_handler(event, context):
     rds_instance_name = os.environ["RDS_INSTANCE_NAME"]
     s3_bucket_name = os.environ["S3_BUCKET_NAME"]
@@ -22,28 +39,7 @@ def lambda_handler(event, context):
     time_copied_up_to_this_run = 0
     writes = 0
 
-    try:
-        s3_client.head_bucket(Bucket=s3_bucket_name)
-    except botocore.exceptions.ClientError as e:
-        raise Exception(f"Unable to access bucket {s3_bucket_name}: {e}")
-
-    try:
-        lrf = s3_client.get_object(Bucket=s3_bucket_name, Key=last_received_file)
-        time_copied_up_to = int(lrf["Body"].read())
-        print(
-            f"Found s3://{s3_bucket_name}/{last_received_file} from previous run; "
-            f"retrieving log files with LastWritten time after {time_copied_up_to}"
-        )
-    except botocore.exceptions.ClientError as e:
-        error_code = int(e.response["ResponseMetadata"]["HTTPStatusCode"])
-        if error_code == 404:
-            print(
-                f"{last_received_file} not found; seems this is the first run. "
-                "All files will be retrieved from RDS."
-            )
-            time_copied_up_to = 0
-        else:
-            raise Exception(f"Unable to access {last_received_file}: {e}")
+    time_copied_up_to = get_last_received_timestamp(s3_client, s3_bucket_name, last_received_file)
 
     for db_log in db_logs["DescribeDBLogFiles"]:
         if int(db_log["LastWritten"]) > time_copied_up_to:
